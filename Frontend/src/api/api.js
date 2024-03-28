8010f9211916dc7716421122eda88dcb27025f17
zubairfawad23@gmail.com <zubairfawad23@gmail.com>
2024-03-29 02:22:23 +0500

    updated URL

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 export const gettopproducts = () => {
-  return axios.get("http://localhost:4000/api/product/getall");
+  return axios.get("https://fyp-deployment-adaptimart-server.vercel.app//api/product/getall");
 };
 
 export const getproductsbyname = (ProductName) => {
-  const url = `http://localhost:4000/api/product/getbyname?name=${ProductName}`;
+  const url = `https://fyp-deployment-adaptimart-server.vercel.app//api/product/getbyname?name=${ProductName}`;
 
   return axios.get(url);
 };
 
 export const getcategories = () => {
-  return axios.get("http://localhost:4000/api/category/getall");
+  return axios.get("https://fyp-deployment-adaptimart-server.vercel.app//api/category/getall");
 };
 
 export const getcategoriesbyname = (CategoryName) => {
-  const url = `http://localhost:4000/api/category/getbyname?name=${CategoryName}`;
+  const url = `https://fyp-deployment-adaptimart-server.vercel.app//api/category/getbyname?name=${CategoryName}`;
 
   return axios.get(url);
 };
